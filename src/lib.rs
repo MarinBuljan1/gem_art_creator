dfba530adca00aa264ae65d7b15978c7ec267011
KRMB-Studios <krmb.studios@gmail.com>
2025-08-04 16:19:48 +1000

    excel columns added

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,17 @@ use std::collections::{HashSet, HashMap};
 
 mod dmc_colors;
 
+fn to_excel_column(num: usize) -> String {
+    let mut s = String::new();
+    let mut n = num;
+    while n > 0 {
+        let rem = (n - 1) % 26;
+        s.insert(0, (b'A' + rem as u8) as char);
+        n = (n - 1) / 26;
+    }
+    s
+}
+
 fn generate_gem_art(image_data: &str, colors: &Vec<Color>) -> Result<(String, String), String> {
     let base64_data = image_data.split(",").nth(1).ok_or("Invalid image data")?;
     let decoded_data = general_purpose::STANDARD.decode(base64_data).map_err(|e| e.to_string())?;
@@ -141,7 +152,7 @@ fn generate_gem_art(image_data: &str, colors: &Vec<Color>) -> Result<(String, St
 
     let mut gem_counts_text = String::new();
     for (i, (floss, count)) in sorted_counts.iter().enumerate() {
-        let letter = (b'A' + i as u8) as char;
+        let letter = to_excel_column(i + 1);
         gem_counts_text.push_str(&format!("({}) #{}: {} gems\n", letter, floss, count));
     }
 
