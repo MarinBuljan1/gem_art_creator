6e80023437d716bced466ad348107c6a91cae812
KRMB-Studios <krmb.studios@gmail.com>
2025-08-06 14:56:35 +1000

    added different sizes

--- src/lib.rs
+++ src/lib.rs
@@ -580,15 +580,21 @@ fn app() -> Html {
                                             <div class="help-modal">
                                                 <h3>{ "Standard Paper Sizes" }</h3>
                                                 <ul>
-                                                    <li>{ "A0: 841 x 1189 mm" }</li>
-                                                    <li>{ "A1: 594 x 841 mm" }</li>
-                                                    <li>{ "A2: 420 x 594 mm" }</li>
-                                                    <li>{ "A3: 297 x 420 mm" }</li>
-                                                    <li>{ "A4: 210 x 297 mm" }</li>
                                                     <li>{ "A5: 148 x 210 mm" }</li>
-                                                    <li>{ "Letter: 215.9 x 279.4 mm" }</li>
-                                                    <li>{ "Legal: 215.9 x 355.6 mm" }</li>
-                                                    <li>{ "Tabloid: 279.4 x 431.8 mm" }</li>
+                                                    <li>{ "A4: 210 x 297 mm" }</li>
+                                                    <li>{ "A3: 297 x 420 mm" }</li>
+                                                    <li>{ "A2: 420 x 594 mm" }</li>
+                                                    <li>{ "A1: 594 x 841 mm" }</li>
+                                                    <li>{ "A0: 841 x 1189 mm" }</li>
+                                                    <li>{ "4x6\": 101.6 x 152.4 mm" }</li>
+                                                    <li>{ "5x5\": 127 x 127 mm" }</li>
+                                                    <li>{ "5x7\": 127 x 177.8 mm" }</li>
+                                                    <li>{ "6x6\": 152.4 x 152.4 mm" }</li>
+                                                    <li>{ "6x8\": 152.4 x 203.2 mm" }</li>
+                                                    <li>{ "8x8\": 203.2 x 203.2 mm" }</li>
+                                                    <li>{ "8x10\": 203.2 x 254 mm" }</li>
+                                                    <li>{ "12x12\": 304.8 x 304.8 mm" }</li>
+                                                    <li>{ "16x20\": 406.4 x 508 mm" }</li>
                                                 </ul>
                                             </div>
                                         }
